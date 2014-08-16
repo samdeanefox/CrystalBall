19fccbc61c0f85c15ebc128bae211dbbe0084cf6
samdeanefox <samdeanefox@gmail.com>
2014-08-16 00:46:23 -0700

    second block
    progress after the second section finished

--- SDFViewController.m
+++ SDFViewController.m
@@ -15,9 +15,11 @@
 @implementation SDFViewController
 
 - (void)viewDidLoad
+//THIS CODE LAUNCHES ONLY ONCE WHEN THE VIEW IS LOADED/
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,5 +30,7 @@
 
 - (IBAction)buttonPressed {
     self.predictionLabel.text = @"YES";
+    self.predictionLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:31];
+    self.predictionLabel.textColor = [UIColor colorWithRed:(188/255.f) green:(149/255.f) blue:(88/255.f) alpha:1.0];
 }
 @end
