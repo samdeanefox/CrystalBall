1a55ac304823b38f068f5e23780405400f0e3229
samdeanefox <samdeanefox@gmail.com>
2014-08-17 16:57:19 -0700

    background image inserted programatically
    Same functionality, now the background image is programmed in, instead
    of being done through the storyboard

--- SDFViewController.m
+++ SDFViewController.m
@@ -19,34 +19,21 @@
 //THIS CODE LAUNCHES ONLY ONCE WHEN THE VIEW IS LOADED/
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     self.crystalBall = [[SDFCrystalBall alloc] init];
     
-    /*self.colors = [[NSArray alloc] initWithObjects: [UIColor blackColor], [UIColor blueColor], [UIColor greenColor], [UIColor grayColor], [UIColor purpleColor], [UIColor redColor], [UIColor yellowColor], [UIColor brownColor], [UIColor magentaColor], [UIColor cyanColor], [UIColor orangeColor], nil];*/
+    UIImage *backgroundImage = [UIImage imageNamed:@"background"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:backgroundImage];
+    [self.view insertSubview:imageView atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)buttonPressed {
     
-    /*NSArray *predictions = [[NSArray alloc] initWithObjects:@"It is certain",@"It is Decidedly so",
-        @"All signs say YES",
-        @"The stars are not aligned",
-        @"The reply is no",
-        @"It is doubtful",
-        @"Better not tell you now",
-        @"Concentrate and ask again",
-        @"Unable to answer now",
-        nil];*/
-    
     self.predictionLabel.text = [self.crystalBall randomPrediction];
     self.predictionLabel.textColor = [self.crystalBall randomColor];
-    //self.predictionLabel.text = @"YES";
-    //self.predictionLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:31];
-    //self.predictionLabel.textColor = [UIColor colorWithRed:(188/255.f) green:(149/255.f) blue:(88/255.f) alpha:1.0];
 }
 @end
